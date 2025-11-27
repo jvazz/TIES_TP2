02195edc7414923df083ff784dcc240529bb8138
Vaz0x <85021857+jvazz@users.noreply.github.com>
2025-11-26 23:50:56 -0300

    Add files via upload

--- assets/js/global/menu-header.js
+++ assets/js/global/menu-header.js
@@ -3,6 +3,6 @@ window.addEventListener("scroll", function(){
 
     // Se o menu estiver aberto, não altera o estilo do header com base no scroll
     if (!header.classList.contains('menu-open')) {
-        header.classList.toggle('scrolling', window.scrollY > 0)
+        header.classList.toggle('scrolling', window.scrollY > 50)
     }
 })
