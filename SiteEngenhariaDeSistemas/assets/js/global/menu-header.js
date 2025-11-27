34b427b9f01b1f05d22347c97b8fafd470a9ba35
Vaz0x <85021857+jvazz@users.noreply.github.com>
2025-11-26 23:56:22 -0300

    Delete SiteEngenhariaDeSistemas

--- SiteEngenhariaDeSistemas/assets/js/global/menu-header.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-
-    // Se o menu estiver aberto, não altera o estilo do header com base no scroll
-    if (!header.classList.contains('menu-open')) {
-        header.classList.toggle('scrolling', window.scrollY > 50)
-    }
-})
