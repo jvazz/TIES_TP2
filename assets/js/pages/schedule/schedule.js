1411bce0ad6af10ef52764ba383f88199b8f6e72
Vaz0x <85021857+jvazz@users.noreply.github.com>
2025-11-28 00:32:03 -0300

    Add files via upload

--- assets/js/pages/schedule/schedule.js
+++ assets/js/pages/schedule/schedule.js
@@ -1,7 +1,9 @@
-const cards = document.querySelectorAll('.general .card');
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.general .card');
 
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        card.classList.toggle('active');
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('active');
+        });
     });
 });
